cff7678f529790ff79ab0973442b19ca783d32cc
sergio-cannata-augeos <sergio.cannata@augeos.it>
2026-01-09 14:49:59 +0100

    Creato con Colab

---
+++ Untitled0.py
@@ -0,0 +1,52 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMZ7pIyFBHNL4REI+J5BXsD",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/AugeosRnD/prova/blob/main/Untitled0.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "import numpy as np"
+      ],
+      "metadata": {
+        "id": "SNi4-dxa5vUm"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print('hello')"
+      ],
+      "metadata": {
+        "id": "m4jd4g165yln"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
